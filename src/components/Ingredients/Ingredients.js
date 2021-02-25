90aea1d039bedeb3e5924a9c42b909b22be8c631
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-02-25 23:27:32 +0500

    firebase post method added

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,11 +7,21 @@ const Ingredients = () => {
   const [ingredients, setIngredients] = useState("");
 
   const addIngredientHandler = (ingredient) => {
-    setIngredients((prevState) => [
-      ...prevState,
-      { ...ingredient, id: Math.random().toString(), ...ingredient },
-    ]);
+    fetch("https://list-app-f6945-default-rtdb.firebaseio.com/lists.json", {
+      method: "POST",
+      body: JSON.stringify(ingredient),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((resData) => {
+        setIngredients((prevState) => [
+          ...prevState,
+          { ...ingredient, id: resData.name, ...ingredient },
+        ]);
+      });
   };
+
+  console.log(ingredients);
 
   return (
     <div className="App">
