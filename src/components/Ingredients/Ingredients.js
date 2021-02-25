f10142dc9650f2befaa304fee81bd4af242abb14
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-02-26 00:47:11 +0500

    fetching data from firebase using useEffect hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import IngredientList from "./IngredientList";
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState("");
+
+  useEffect(() => {
+    fetch("https://list-app-f6945-default-rtdb.firebaseio.com/lists.json")
+      .then((res) => res.json())
+      .then((getdata) => {
+        const arr = [];
+        for (const key in getdata) {
+          arr.push({ ...getdata[key], id: key });
+        }
+        setIngredients(arr);
+      });
+  },[]);
 
   const addIngredientHandler = (ingredient) => {
     fetch("https://list-app-f6945-default-rtdb.firebaseio.com/lists.json", {
@@ -20,8 +32,6 @@ const Ingredients = () => {
         ]);
       });
   };
-
-  console.log(ingredients);
 
   return (
     <div className="App">
